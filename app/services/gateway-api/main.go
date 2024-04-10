5a6ac41bdb6bb98fc7cb1ecabcfa43fe360b9bfd
Egor Stepanenko <st3pegor@gmail.com>
2024-04-10 13:13:02 +0700

    basic main

---
+++ app/services/gateway-api/main.go
@@ -0,0 +1,134 @@
+package main
+
+import (
+	"errors"
+	"fmt"
+	"github.com/St3plox/Gopher-storage/business/web/v1/debug"
+	"github.com/St3plox/Gopher-storage/foundation/logger"
+	"github.com/ardanlabs/conf/v3"
+	"github.com/rs/zerolog"
+	defaultLog "log"
+	"net/http"
+	"os"
+	"runtime"
+	"time"
+)
+
+var build = "develop"
+
+func main() {
+	log := logger.New("GATEWAY - SERVICE")
+
+	if err := run(log); err != nil {
+		log.Error().Err(err).Msg("startup")
+		os.Exit(1)
+	}
+}
+
+func run(log *zerolog.Logger) error {
+
+	// -------------------------------------------------------------------------
+	// GOMAXPROCS
+
+	log.Info().Str("startup", "GOMAXPROCS").Int("GOMAXPROCS", runtime.
+		GOMAXPROCS(0)).
+		Str("BUILD", build).
+		Msg("startup")
+
+	// -------------------------------------------------------------------------
+	// Configuration
+
+	cfg := struct {
+		conf.Version
+		Web struct {
+			ReadTimeout     time.Duration `conf:"default:5s"`
+			WriteTimeout    time.Duration `conf:"default:10s"`
+			IdleTimeout     time.Duration `conf:"default:120s"`
+			ShutdownTimeout time.Duration `conf:"default:20s,mask"`
+			APIHost         string        `conf:"default::8080"`
+			DebugHost       string        `conf:"default::4001"`
+		}
+	}{
+		Version: conf.Version{
+			Build: build,
+			Desc:  "copyright information here",
+		},
+	}
+
+	const prefix = "GATEWAY"
+	help, err := conf.Parse(prefix, &cfg)
+
+	if err != nil {
+		if errors.Is(err, conf.ErrHelpWanted) {
+			fmt.Println(help)
+			return nil
+		}
+		return fmt.Errorf("parsing config: %w", err)
+	}
+
+	// -------------------------------------------------------------------------
+	// App Starting
+
+	log.Info().Str("version", build).Msg("starting service")
+	defer log.Info().Msg("shutdown complete")
+
+	out, err := conf.String(&cfg)
+	if err != nil {
+		return fmt.Errorf("generating config for output: %w", err)
+	}
+	log.Info().Str("config", out).Msg("startup")
+
+	// -------------------------------------------------------------------------
+	// Start Debug Service
+
+	log.Info().
+		Str("status", "debug v1 router started").
+		Str("host", cfg.Web.DebugHost).
+		Msg("startup")
+
+	go func() {
+		if err := http.ListenAndServe(cfg.Web.DebugHost, debug.StandardLibraryMux()); err != nil {
+			log.Error().
+				Str("status", "debug v1 router closed").
+				Str("host", cfg.Web.DebugHost).
+				Err(err).
+				Msg("shutdown")
+
+		}
+	}()
+
+	// -------------------------------------------------------------------------
+	// Start API Service
+
+	mux := http.NewServeMux()
+	mux.HandleFunc("GET /path/", func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprint(w, "got path\n")
+	})
+
+	errorLogger := zerolog.New(os.Stderr).With().Timestamp().Logger()
+	api := http.Server{
+		Addr:         cfg.Web.APIHost,
+		Handler:      http.DefaultServeMux,
+		ReadTimeout:  cfg.Web.ReadTimeout,
+		WriteTimeout: cfg.Web.WriteTimeout,
+		IdleTimeout:  cfg.Web.IdleTimeout,
+		ErrorLog:     defaultLog.New(&errorLogger, "", 0),
+	}
+
+	serverErrors := make(chan error, 1)
+	go func() {
+		log.Info().
+			Str("status", "api router started").
+			Str("host", api.Addr).
+			Msg("startup")
+		serverErrors <- api.ListenAndServe()
+	}()
+
+	// -------------------------------------------------------------------------
+	// Shutdown
+
+	select {
+	case err := <-serverErrors:
+		return fmt.Errorf("server error: %w", err)
+	}	
+}
